f33a7d997de210d951942767e540d4d4146f5683
vimalraj <vimalshanmugaraj@gmail.com>
2025-02-12 13:41:45 +0530

    save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -5,9 +5,11 @@ if(!cart){
         {
             productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
             quantity: 2,
+            deliveryOptionId : 1
           }, {
             productId: '54e0eccd-8f36-462b-b68a-8182611d9add',
             quantity: 2,
+            deliveryOptionId : 1
           }
     ];
 }
@@ -33,7 +35,8 @@ export function addToCart(productId){
     else{
     cart.push({
         productId: productId,
-        quantity: 1
+        quantity: 1,
+        deliveryOptionId : 1
     });
     }
 
